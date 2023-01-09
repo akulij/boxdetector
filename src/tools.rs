574fdd9486761f181853e72410dc05c5362539c1
akulij <48361256+akulij@users.noreply.github.com>
2023-01-09 17:12:57 +0500

    add wow64 checker

--- src/tools.rs
+++ src/tools.rs
@@ -7,6 +7,33 @@ pub fn should_panic_release(should_me: bool) -> bool {
     should_me
 }
 
+#[cfg(windows)]
+pub fn is_wow64() -> bool {
+    use winapi::um::libloaderapi::GetModuleHandleA;
+    use winapi::um::libloaderapi::GetProcAddress;
+    use winapi::um::processthreadsapi::GetCurrentProcess;
+    use winapi::um::winnt::HANDLE;
+
+    let mut res = false;
+
+    unsafe {
+        let func_native = GetProcAddress(
+            GetModuleHandleA("kernel32".as_ptr() as *const i8),
+            "IsWow64Process".as_ptr() as *const i8,
+        );
+        if func_native as u32 != 0 {
+            let fnptr = func_native as *const ();
+            let fnptr: fn(HANDLE, &mut bool) -> u32 = std::mem::transmute(fnptr);
+            if fnptr(GetCurrentProcess(), &mut res) != 0 {
+                return res;
+            } else {
+                return false;
+            }
+        }
+    }
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
