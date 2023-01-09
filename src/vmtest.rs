0f970a1d9361ffa4cc9443f1f66d65815ef7a22d
akulij <48361256+akulij@users.noreply.github.com>
2023-01-09 17:16:29 +0500

    create common vm signs detector function

--- src/vmtest.rs
+++ src/vmtest.rs
@@ -8,3 +8,14 @@ pub fn is_inside_vm() -> bool {
 
     flag
 }
+
+fn check_common_vm() -> bool {
+    let mut flag = false;
+
+    flag |= check_disk_size();
+    flag |= check_sleep_patch();
+    flag |= check_cpu_count();
+    flag |= check_native_vhd_boot();
+
+    flag
+}
